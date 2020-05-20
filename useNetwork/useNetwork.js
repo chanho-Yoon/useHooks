2a528ecc795d6bfebb289ad9f52e9201fc44f7dd
chanho-Yoon <chanho.yoon.1993@gmail.com>
2020-05-20 21:04:57 +0900

    add import package

--- useNetwork/useNetwork.js
+++ useNetwork/useNetwork.js
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react';
 export const useNetwork = (onChange) => {
   const [status, setStatus] = useState(navigator.onLine);
   const handleChange = () => {
--- useTitle/useTitle.js
+++ useTitle/useTitle.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 export const useTitle = () => {
   const [title, setTitle] = useState(null);
 
